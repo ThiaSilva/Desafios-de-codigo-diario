0b3ad183ab0cbde235a8f3e7bf33753baf368694
Thiago Carvalho <90474673+ThiaSilva@users.noreply.github.com>
2025-05-28 11:36:05 -0300

    Update and rename 1001 - Extremamente Básico.js to 1001 - Extremamente Básico.py

--- Desafios de Logica/Dia 2/1001 - Extremamente Básico.js
+++
@@ -1,9 +0,0 @@
-var input = require('fs').readFileSync('/dev/stdin', 'utf8');
-var lines = input.split('\n');
-
-let A = parseInt(lines[0]);
-let B = parseInt(lines[1]);
-
-let X = A + B;
-
-console.log(`X = ${X}`);
---
+++ Desafios de Logica/Dia 2/1001 - Extremamente Básico.py
@@ -0,0 +1,6 @@
+A = int(input()) # Entrada numero A
+B = int(input()) # Entrada numero B
+
+X = A + B # Variavel de soma
+
+print(f"X = {X}")
